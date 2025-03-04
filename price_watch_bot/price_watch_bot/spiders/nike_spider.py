d795ff9fece11994fbefa1964a8f2b012fb83165
Kaan Erol <kaanerol395@gmail.com>
2025-03-04 14:33:40 +0300

    user provided URL

--- price_watch_bot/price_watch_bot/spiders/nike_spider.py
+++ price_watch_bot/price_watch_bot/spiders/nike_spider.py
@@ -1,22 +1,25 @@
 import scrapy
-import re
+from price_watch_bot.items import ProductItem
 
 class NikeSpiderSpider(scrapy.Spider):
     name = "nike_spider"
     allowed_domains = ["www.nike.com"]
+
+    def __init__(self, product_url=None, *args, **kwargs):
+        super(NikeSpiderSpider, self).__init__(*args, **kwargs)
+        if product_url:
+            self.start_urls = [product_url]
+        else:
+            raise ValueError("Lütfen bir ürün URL'si girin!")
+    
     start_urls = ["https://www.nike.com/tr/t/dunk-low-retro-ayakkab%C4%B1s%C4%B1-mhrtZC/DD1391-103"]
 
     def parse(self, response):
-        product_name = response.xpath('//h1[@id="pdp_product_title"]/text()').get()
+        item = ProductItem()
 
-        product_price = response.xpath('//span[@data-testid="currentPrice-container"]/text()').get()
-        if product_price:
-            product_price = re.sub(r'[₺\s]', '', product_price)
-            product_price = product_price.replace('.', '')
-            product_price = product_price.replace(',', '.')
+        item['url'] = response.url
+        item['product_name'] = response.xpath('//h1[@id="pdp_product_title"]/text()').get()
+        item['product_price'] = response.xpath('//span[@data-testid="currentPrice-container"]/text()').get()
+        item['brand'] = "Nike"
 
-        yield {
-            'product_name': product_name,
-            'product_price': product_price,
-            'url': response.url,
-        }
+        yield item
