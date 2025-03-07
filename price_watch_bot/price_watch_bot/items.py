8a4b25711d6b39439435c19f7bd39bc79e0b1718
Kaan Erol <kaanerol395@gmail.com>
2025-03-07 23:50:40 +0300

    remove brand and serialize_price

--- price_watch_bot/price_watch_bot/items.py
+++ price_watch_bot/price_watch_bot/items.py
@@ -4,12 +4,14 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+import re
 
-def serialize_price(value):
-    return float(value.replace('₺', '').replace(',', '.').strip())
+class BookscraperItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field()
+    pass
 
 class ProductItem(scrapy.Item):
     url = scrapy.Field()
     product_name = scrapy.Field()
-    product_price = scrapy.Field(serializer=serialize_price)
-    brand = scrapy.Field()
+    product_price = scrapy.Field()
