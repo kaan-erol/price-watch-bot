158e9616ae02a48f5b8bb100c2867e1fb3748a20
Kaan Erol <kaanerol395@gmail.com>
2025-03-07 23:47:29 +0300

    add start url and remove brand

--- price_watch_bot/price_watch_bot/spiders/nike_spider.py
+++ price_watch_bot/price_watch_bot/spiders/nike_spider.py
@@ -4,13 +4,7 @@ from price_watch_bot.items import ProductItem
 class NikeSpiderSpider(scrapy.Spider):
     name = "nike_spider"
     allowed_domains = ["www.nike.com"]
-
-    def __init__(self, product_url=None, *args, **kwargs):
-        super(NikeSpiderSpider, self).__init__(*args, **kwargs)
-        if product_url:
-            self.start_urls = [product_url]
-        else:
-            raise ValueError("Lütfen bir ürün URL'si girin!")
+    start_urls = ["https://www.nike.com/tr/t/dunk-low-retro-ayakkab%C4%B1s%C4%B1-mhrtZC/DD1391-103"]
 
     def parse(self, response):
         item = ProductItem()
@@ -18,6 +12,5 @@ class NikeSpiderSpider(scrapy.Spider):
         item['url'] = response.url
         item['product_name'] = response.xpath('//h1[@id="pdp_product_title"]/text()').get()
         item['product_price'] = response.xpath('//span[@data-testid="currentPrice-container"]/text()').get()
-        item['brand'] = "Nike"
 
         yield item
