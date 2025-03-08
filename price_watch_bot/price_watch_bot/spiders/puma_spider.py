8865d258b57236094d9f226081a559dce93b0091
Kaan Erol <kaanerol395@gmail.com>
2025-03-08 18:49:34 +0300

    puma spider

--- price_watch_bot/price_watch_bot/spiders/puma_spider.py
+++ price_watch_bot/price_watch_bot/spiders/puma_spider.py
@@ -1,10 +1,16 @@
 import scrapy
-
+from price_watch_bot.items import ProductItem
 
 class PumaSpiderSpider(scrapy.Spider):
     name = "puma_spider"
     allowed_domains = ["tr.puma.com"]
-    start_urls = ["https://tr.puma.com"]
+    start_urls = ["https://tr.puma.com/tifosi-unisex-ayakkabi-397454-05.html"]
 
     def parse(self, response):
-        pass
+        item = ProductItem()
+
+        item['url'] = response.url
+        item['product_name'] = response.xpath('//h1[@class="page-title"]/text()').get()
+        item['product_price'] = response.xpath('//span[@class="price"]/text()').get()
+
+        yield item
