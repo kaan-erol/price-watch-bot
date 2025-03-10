dcd3089fa45d18cdf7f3e4d9a5ea92808c2c9514
Kaan Erol <kaanerol395@gmail.com>
2025-03-10 18:42:16 +0300

    scraping urls from database

--- price_watch_bot/price_watch_bot/spiders/puma_spider.py
+++ price_watch_bot/price_watch_bot/spiders/puma_spider.py
@@ -1,10 +1,31 @@
 import scrapy
 from price_watch_bot.items import ProductItem
+import mysql.connector
+import sys
+sys.path.append('C:/Users/kaane/Repository/price-watch-bot/price_watch_bot/price_watch_bot')
+from config import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE
 
 class PumaSpiderSpider(scrapy.Spider):
     name = "puma_spider"
     allowed_domains = ["tr.puma.com"]
-    start_urls = ["https://tr.puma.com/tifosi-unisex-ayakkabi-397454-05.html"]
+
+    def start_requests(self):
+        conn = mysql.connector.connect(
+            host=MYSQL_HOST,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+            database=MYSQL_DATABASE
+        )
+        cur = conn.cursor()
+
+        cur.execute("SELECT url FROM products")
+        urls = cur.fetchall()
+        
+        for url in urls:
+            yield scrapy.Request(url=url[0], callback=self.parse)
+
+        cur.close()
+        conn.close()
 
     def parse(self, response):
         item = ProductItem()
