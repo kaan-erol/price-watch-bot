8637095f913ba846b6b6351e4f8aacdea024c7f6
Kaan Erol <kaanerol395@gmail.com>
2025-03-13 19:47:59 +0300

    nike spider db connection

--- price_watch_bot/price_watch_bot/spiders/nike_spider.py
+++ price_watch_bot/price_watch_bot/spiders/nike_spider.py
@@ -1,12 +1,32 @@
 import scrapy
 from price_watch_bot.items import ProductItem
+import mysql.connector
+from price_watch_bot.config import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE
 
 class NikeSpiderSpider(scrapy.Spider):
     name = "nike_spider"
     allowed_domains = ["www.nike.com"]
-    start_urls = ["https://www.nike.com/tr/t/dunk-low-retro-ayakkab%C4%B1s%C4%B1-mhrtZC/DD1391-103"]
+
+    def __init__(self):
+        # Initialize database connection when the spider starts.
+        self.conn = mysql.connector.connect(
+            host=MYSQL_HOST,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+            database=MYSQL_DATABASE
+        )
+        self.cur = self.conn.cursor()
+
+    def start_requests(self):
+        # Fetch product URLs from the database and start scraping each one.
+        self.cur.execute("SELECT url FROM products")
+        urls = self.cur.fetchall()
+        
+        for url in urls:
+            yield scrapy.Request(url=url[0], callback=self.parse)
 
     def parse(self, response):
+        # Extract product details
         item = ProductItem()
 
         item['url'] = response.url
@@ -14,3 +34,8 @@ class NikeSpiderSpider(scrapy.Spider):
         item['product_price'] = response.xpath('//span[@data-testid="currentPrice-container"]/text()').get()
 
         yield item
+
+    def close_spider(self, spider):
+        # Close database connection
+        self.cur.close()
+        self.conn.close()
